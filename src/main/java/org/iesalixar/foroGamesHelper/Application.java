3f5d73449a919151c3c6adc48524bd51e0381a45
Guillermo Moreno <g.moreno.rodriguez@accenture.com>
2022-06-12 20:56:14 +0200

    Properties base de datos y main para correr la aplicacion

---
+++ src/main/java/org/iesalixar/foroGamesHelper/Application.java
@@ -0,0 +1,15 @@
+package org.iesalixar.foroGamesHelper;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
